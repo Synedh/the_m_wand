11aa75a2b480ab64d9651c09f58f26329a357f4a
Guillian VANSTEENE <guigeek94@hotmail.fr>
2018-04-17 19:20:12 +0200

    Add serializable object for levels

---
+++ Assets/Scripts/Levels/Level.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class Level : ScriptableObject{
+   
+    public string goal;
+    public int valueGoal;
+    public List<Wave> eventList;
+
+    public Level()
+    {
+        eventList = new List<Wave>();
+        goal = "Ennemies";
+        valueGoal = 20;
+    }
+
+    public void addWave()
+    {
+        eventList.Add(new Wave());
+    }
+
+    public void addDialog()
+    {
+        eventList.Add(new Wave("okBox"));
+    }
+}
+
+
+[Serializable]
+public class Wave
+{
+    public string type;
+    public List<Enemy> enemies;
+
+    public Wave()
+    {
+        type = "Enemies";
+        enemies = new List<Enemy>();
+        enemies.Add(new Enemy());
+        enemies.Add(new Enemy());
+    }
+
+    public Wave(String type)
+    {
+        this.type = type;
+    }
+}
+
+[Serializable]
+public class Enemy
+{
+    public int speed;
+    public int attack;
+    public int spawnpoint;
+    public int depth;
+
+    public Enemy()
+    {
+        speed = 1;
+        attack = 1;
+        spawnpoint = 0;
+        depth = 1;
+    }
+
+}
---
+++ Assets/Scripts/Levels/LevelScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LevelScript : MonoBehaviour {
+
+
+    public Level lvl;
+
+	// Use this for initialization
+	void Start () {
+
+        this.lvl = new Level();
+        lvl.addDialog();
+        lvl.addWave();
+        lvl.addWave();
+        lvl.addWave();
+
+
+
+
+        string json = JsonUtility.ToJson(lvl);
+
+        print(json);
+
+        AssetDatabase.CreateAsset(lvl, "Assets/Levels/testlevel12.asset");
+
+    }
+}
